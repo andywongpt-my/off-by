4682e1f36ef17f2aa0dd234bca97f26f548f9b4c
Andy <andywongcg@gmail.com>
2026-09-12 15:55:12 +0800

    Remove duplicate migration version

--- supabase/migrations/202609120002_tighten_off_by_service_role.sql
+++
@@ -1,5 +0,0 @@
-revoke all on table public.off_by_attempts from service_role;
-grant select, insert on table public.off_by_attempts to service_role;
-
-revoke all on sequence public.off_by_attempts_id_seq from service_role;
-grant usage, select on sequence public.off_by_attempts_id_seq to service_role;
